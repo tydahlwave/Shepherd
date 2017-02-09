48b05606f2d79835ce2385b7005a964198ca351f
Michael Cantrell <micantre@calpoly.edu>
2017-02-08 21:31:22 -0800

    Groundwork Laid For Spatial Data Structure

--- src/PhysicsController.h
+++ src/PhysicsController.h
@@ -22,8 +22,9 @@ public:
     void KeyPressed(World *world, int windowWidth, int windowHeight, int key, int action);
     void MouseMoved(World *world, int windowWidth, int windowHeight, double mouseX, double mouseY);
     void MouseClicked(World *world, double mouseX, double mouseY, int key, int action);
-    bool isLyingInCone(btVector3 x, btVector3 t, btVector3 b, float aperture);
     
+private:
+    bool isLyingInCone(btVector3 x, btVector3 t, btVector3 b, float aperture);
     long LeftClickPressTime;
     long RightClickPressTime;
 };
