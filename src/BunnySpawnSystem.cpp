3e7267f526938e4b7ac2b59b4b76e091e2c66806
tiedie211 <tchristiand@gmail.com>
2017-02-16 15:37:33 -0800

    Increased sheep count and changed spawn location to 0,0,0.

--- src/BunnySpawnSystem.cpp
+++ src/BunnySpawnSystem.cpp
@@ -63,8 +63,8 @@ void BunnySpawnSystem::Update(float deltaTime, World *world, GameObject *p) {
         {
 			std::random_device rd;
 			std::mt19937 mt(rd());
-			std::uniform_real_distribution<float> distX(-15.0f, -25.0f);
-			std::uniform_real_distribution<float> distZ(-15.0f, -25.0f);
+			std::uniform_real_distribution<float> distX(-5.0f, 5.0f);
+			std::uniform_real_distribution<float> distZ(-5.0f, 5.0f);
 
             //randX = rand() % 50 - 25;
             //randZ = rand() % 50 - 25;
