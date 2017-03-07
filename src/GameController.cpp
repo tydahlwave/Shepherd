a027738db13fa3693d6705f8a07126f501f7d05b
Michael Cantrell <micantre@calpoly.edu>
2017-03-07 12:43:06 -0800

    fixed lighting (removed edge detection)

--- src/GameController.cpp
+++ src/GameController.cpp
@@ -340,7 +340,7 @@ void GameController::LoadState() {
         GameObject *skybox = EntityFactory::createSkybox(&world, resourceDir);
         
 		// Create terrain
-		terrain = EntityFactory::createTerrain(&world, resourceDir, SIMPLEX_TERRAIN, 1081, glm::vec3(0, 0, 0));
+		terrain = EntityFactory::createTerrain(&world, resourceDir, SIMPLEX_TERRAIN, 256, glm::vec3(0, 0, 0));
 		terrain->transform->SetScale(glm::vec3(5, 5, 5));
 		
 		//create skybox
